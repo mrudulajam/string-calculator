c26f2b7369f798522219e191bff4193cef5681f3
Mrudula <jamkhedkarmrudula@gmail.com>
2025-01-29 12:40:12 +0530

    Handled single number case in add() function

--- string_calculator.py
+++ string_calculator.py
@@ -1,10 +1,4 @@
 def add(numbers: str) -> int:
-    return 0
-    
-    num_list = map(int, numbers.split(','))
-    return sum(num_list)
-
-# Test cases
-print(add(""))
-print(add("1"))
-print(add("1,5"))
+    if not numbers:
+        return 0
+    return int(numbers)
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -2,8 +2,8 @@ import unittest
 from string_calculator import add  # Import the function from your main file
 
 class TestStringCalculator(unittest.TestCase):
-    def test_empty_string(self):
-        self.assertEqual(add(""), 0)
+	def test_single_number(self):
+		self.assertEqual(add("1"), 1)
 
 if __name__ == "__main__":
-    unittest.main()
+	unittest.main()
