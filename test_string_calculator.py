b529502a910375a56eb2b7b5edc71998294112b5
Mrudula <jamkhedkarmrudula@gmail.com>
2025-01-29 12:13:19 +0530

    Added initial test structure for String Calculator

---
+++ test_string_calculator.py
@@ -0,0 +1,10 @@
+
+import unittest
+from string_calculator import add  # Import the function from your main file
+
+class TestStringCalculator(unittest.TestCase):
+    def test_empty_string(self):
+        self.assertEqual(add(""), 0)
+
+if __name__ == "__main__":
+    unittest.main()
