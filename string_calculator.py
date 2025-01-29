705b6262c8f656e8ecdd2932c874ea78b5cf7247
Mrudula <jamkhedkarmrudula@gmail.com>
2025-01-29 13:06:30 +0530

    Handled multiple numbers in add() function

--- string_calculator.py
+++ string_calculator.py
@@ -1,2 +1,17 @@
 def add(numbers: str) -> int:
     return 0
+
+def add(numbers: str) -> int:
+    if not numbers:
+        return 0
+    return int(numbers)
+
+def add(numbers: str) -> int:
+    if not numbers:
+        return 0
+    return sum(map(int, numbers.split(",")))
+
+def add(numbers: str) -> int:
+    if not numbers:
+        return 0
+    return sum(map(int, numbers.split(",")))
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -2,8 +2,16 @@ import unittest
 from string_calculator import add  # Import the function from your main file
 
 class TestStringCalculator(unittest.TestCase):
-    def test_empty_string(self):
-        self.assertEqual(add(""), 0)
+	def test_empty_string(self):
+		self.assertEqual(add(""), 0)
+	def test_single_number(self):
+		self.assertEqual(add("1"), 1)
+	def test_two_numbers(self):
+		self.assertEqual(add("1,5"), 6)
+	def test_two_numbers(self):
+		self.assertEqual(add("1,5"), 6)
+	def test_multiple_numbers(self):
+		self.assertEqual(add("1,2,3,4,5"), 15)
 
 if __name__ == "__main__":
-    unittest.main()
+	unittest.main()
